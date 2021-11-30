c0d6549c1176b8adb154761cb9f58726a0b85f27
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-11-30 01:34:38 -0300

    Re-enable gr.uom.java.xmi.diff and TestAllRefactorings

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -1,13 +1,10 @@
 package org.refactoringminer.test;
 
-import org.junit.Ignore;
+import org.junit.Test;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
 import org.refactoringminer.test.RefactoringPopulator.Refactorings;
 import org.refactoringminer.test.RefactoringPopulator.Systems;
 
-import org.junit.Test;
-
-@Ignore
 public class TestAllRefactorings {
 
 	@Test
