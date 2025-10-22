21e1d407d9576874da3758bdc58ae8a7b2f40332
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-10-22 10:13:57 -0400

    Update test with new TPs and FPs after supporting default values and
    multiple-variable assignment

--- src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
@@ -26,6 +26,6 @@ public class TestPythonDatasetRefactorings {
 					.or(Refactorings.ExtractVariable.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.preparePythonRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(99, 3, 1);
+		test.assertExpectationsWithGitHubAPI(104, 9, 1);
 	}
 }
