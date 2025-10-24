d2ff253462e964dbce2d060c47d7fcaae757c36f
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-10-23 21:08:23 -0400

    kwargs fix eliminates two false positives

--- src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
@@ -29,6 +29,6 @@ public class TestPythonDatasetRefactorings {
 					.or(Refactorings.MoveAttribute.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.preparePythonRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(135, 3, 1);
+		test.assertExpectationsWithGitHubAPI(135, 1, 1);
 	}
 }
