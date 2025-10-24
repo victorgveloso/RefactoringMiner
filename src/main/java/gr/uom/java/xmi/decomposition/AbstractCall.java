cabe6bea5c8105386ca61bb51bf4c1fe3fc37c90
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-10-24 19:42:55 -0400

    Improve testMethodCheck() rule for JUnit3

--- src/main/java/gr/uom/java/xmi/decomposition/AbstractCall.java
+++ src/main/java/gr/uom/java/xmi/decomposition/AbstractCall.java
@@ -1500,7 +1500,7 @@ public abstract class AbstractCall extends LeafExpression {
 	}
 
 	public boolean isAssertion() {
-		return getName().startsWith("assert") || getName().equals("fail");
+		return getName().startsWith("assert") || getName().equals("fail") || getName().startsWith("expect");
 	}
 
 	public enum StatementCoverageType {
