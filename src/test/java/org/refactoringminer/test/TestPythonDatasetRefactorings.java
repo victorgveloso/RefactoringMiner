8903efca18a8422ccccf6445ed69f9f9da262978
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-11-17 06:55:29 -0500

    Add 1 commit from ffmpeg-python in Python commit tests

--- src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
@@ -40,6 +40,6 @@ public class TestPythonDatasetRefactorings {
 					.or(Refactorings.MoveCode.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.preparePythonRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(279, 4, 1);
+		test.assertExpectationsWithGitHubAPI(286, 4, 1);
 	}
 }
