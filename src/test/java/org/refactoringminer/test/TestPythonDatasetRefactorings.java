8e7e533cb99e087c2cb3a5fa3ae632129e7020f1
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-11-17 21:35:00 -0500

    Update test

--- src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
@@ -40,6 +40,6 @@ public class TestPythonDatasetRefactorings {
 					.or(Refactorings.MoveCode.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.preparePythonRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(314, 4, 1);
+		test.assertExpectationsWithGitHubAPI(313, 3, 1);
 	}
 }
