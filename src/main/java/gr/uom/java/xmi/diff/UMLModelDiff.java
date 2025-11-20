7e21ee0b70ed75c946563b63096b7d76c353b488
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-11-20 15:17:22 -0500

    Avoid super call as the only mapping in Extract and Move Method

--- src/main/java/gr/uom/java/xmi/diff/UMLModelDiff.java
+++ src/main/java/gr/uom/java/xmi/diff/UMLModelDiff.java
@@ -6357,6 +6357,9 @@ public class UMLModelDiff {
 						}
 					}
 				}
+				if(invocation1.isSuperCall() && invocation2.isSuperCall()) {
+					return false;
+				}
 			}
 			AbstractCodeFragment fragment1 = mappingList.get(0).getFragment1();
 			AbstractCodeFragment fragment2 = mappingList.get(0).getFragment2();
