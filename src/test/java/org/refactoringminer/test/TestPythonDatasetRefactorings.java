257dff3c84571c2131a3300049f9a1dc92902d4e
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-11-20 15:47:06 -0500

    Add 2 commits in Python commit tests

--- src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestPythonDatasetRefactorings.java
@@ -42,6 +42,6 @@ public class TestPythonDatasetRefactorings {
 					.or(Refactorings.RemoveMethodAnnotation.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.preparePythonRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(448, 5, 1);
+		test.assertExpectationsWithGitHubAPI(451, 5, 1);
 	}
 }
